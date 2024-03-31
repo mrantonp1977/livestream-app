9b745e228d781debbc037766959c696394594d28
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-31 23:52:37 +0300

    complete chat message and community

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+// import stc from "string-to-color";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const stringToColor = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = '#';
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xFF;
+    color += ( "0" + value.toString(16)).substr(-2);
+  }
+  return color;
+};
