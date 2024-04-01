d4b1807e2af148dd4cd6453cde297536c6fd1cbf
Antonis Papaioannou <mrantonp@gmail.com>
2024-04-01 13:45:12 +0300

    complete info modal uploadthing

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,6 +10,7 @@ import { Chat, ChatSkeleton } from './chat';
 import { ChatToggle } from './chat-toggle';
 
 import { Header, HeaderSkeleton } from './header';
+import { InfoCard } from './info-card';
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -60,6 +61,12 @@ export const StreamPlayer = ({
             imageUrl={user.imageUrl}
             isFollowing={isFollowing}
             name={stream.name}
+          />
+          <InfoCard 
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            name={stream.name}
+            thumbnailUrl={stream.thumbnailUrl}
           />
         </div>
         <div className={cn(
